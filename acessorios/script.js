e817d6e1f9b8fd1496b36ccc1d5155b8462d6de2
Francisco Jeronimo <franciscojeronimo1@hotmail.com>
2023-12-28 20:31:36 -0300

    finish?

--- acessorios/script.js
+++ acessorios/script.js
@@ -1,1 +1,20 @@
 AOS.init();
+
+/*    Passar para o lado sosinho     */
+
+let count = 1
+document.getElementById("radio1").checked = true
+
+setInterval(function(){
+    nextImage()
+},2000)
+
+function nextImage() {
+    count++;
+    if(count>4){
+        count = 1
+    }
+
+    document.getElementById("radio"+count).checked = true
+
+}
